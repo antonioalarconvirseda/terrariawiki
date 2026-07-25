155bfbc26d50cc1a7272a76d47c6bf77fe2ffb04
AAV <antonioalarconvirseda@hotmail.com>
2026-07-25 18:39:58 +0200

    feat: register CategoryViewModel in Koin with ItemCategory parameter

--- app/src/main/java/com/terrariawiki/features/items/di/ItemsModule.kt
+++ app/src/main/java/com/terrariawiki/features/items/di/ItemsModule.kt
@@ -4,7 +4,9 @@ import com.terrariawiki.features.items.data.ItemsRepository
 import com.terrariawiki.features.items.data.ItemsRepositoryImpl
 import com.terrariawiki.features.items.domain.GetItemByNameUseCase
 import com.terrariawiki.features.items.domain.GetItemsUseCase
+import com.terrariawiki.features.items.domain.ItemCategory
 import com.terrariawiki.features.items.domain.SearchItemsUseCase
+import com.terrariawiki.features.items.ui.CategoryViewModel
 import com.terrariawiki.features.items.ui.ItemDetailViewModel
 import com.terrariawiki.features.items.ui.ItemsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -19,4 +21,5 @@ val itemsModule = module {
 
     viewModel { ItemsViewModel(get(), get()) }
     viewModel { ItemDetailViewModel(get()) }
+    viewModel { params -> CategoryViewModel(params.get<ItemCategory>(), get()) }
 }
